c72fb86c63ed8a6c9f4e0efc4bd07476b950009d
Miguel Evangelista <miguelbranagan30@gmail.com>
2024-02-28 19:38:03 -0400

    resetting app component for admin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import AdminTeam from './components/modal/AdminTeam';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+
+import Footer from './components/Footer';
 
 function App() {
 	return (
 		<>
 			<div className='flex flex-col min-h-screen bg-primary text-base-100'>
-				<AdminTeam />
+				{/* header */}
+
+				<main className=''>
+					<BrowserRouter>
+						<Routes>
+							<Route path='/' element={<Home />} />
+						</Routes>
+					</BrowserRouter>
+
+					{/* <main className=' px-[30px] space-y-4 text-base-100 md:px-[45px] min-h-screen'> */}
+				</main>
+				{/* footer */}
+				<Footer />
 			</div>
 		</>
 	);
